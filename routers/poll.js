439df2dacbc032c860c2113396170702b6a6fec9
otaree <deep.yen.ur.hash@gmail.com>
2018-03-11 11:44:38 +0530

    Added a poll.pug file

--- routers/poll.js
+++ routers/poll.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
@@ -16,14 +17,37 @@ router.get('/all', (req, res, next) => {
         }).catch(next);
 });
 
-router.get('/:id',  (req, res) => {
-    Poll.findById(req.params.id)
+router.get('/:id', requireLogin,  (req, res) => {
+    Poll.findById(req.params.id).populate('author')
         .then(poll => {
-            res.render('poll', {title: 'Poll', poll: poll, user: req.session.userInfo});
+            if (poll.voters.indexOf(req.session.userInfo._id) === -1) {
+                res.render('poll_vote', {title: 'Poll', poll: poll, user: req.session.userInfo});
+            } else {
+                res.render('poll', {title: 'Poll', poll: poll, user: req.session.userInfo});
+            }
+            
         });
 });
 
-router.post('/:id', (req, res) => {});
+router.post('/vote/:id', requireLogin, (req, res, next) => {
+    if (req.body.id !== undefined) {
+        Poll.findOneAndUpdate(
+            { 'options._id': mongoose.Types.ObjectId(req.body.id)},
+            { 
+                "$inc": { "options.$.votes": 1},
+                "$push": { "voters": req.session.userInfo._id }
+             },
+            {new: true}
+        ).populate('author').then(poll => {
+            res.render('poll', {title: 'Poll', poll: poll, user: req.session.userInfo});
+        }).catch(err => next(new Error('Something wrong with the server')));
+    } else {
+        Poll.findById(req.params.id)
+        .then(poll => {
+            res.render('poll_vote', {title: 'Poll', poll: poll, user: req.session.userInfo, error: new Error("Please Select a Option")});
+        });
+    }
+});
 
 
 
